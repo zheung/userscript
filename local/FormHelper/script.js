a105c8c2d41b9e14e517d492e893cf53b36a9a68
DanoR <zheung@vip.qq.com>
2017-01-04 17:18:57 +0800

    新增 (未关联)填表助手

---
+++ local/FormHelper/script.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         FormHelper
+// @description  填表助手
+// @version      0.0.0
+// @namespace    DanoR Script
+// @author       DanoR
+// @run-at       document-end
+// @grant        none
+// @include      *
+// ==/UserScript==
+
+if(self == top) { (function() {
+	console.log('FromHelper Gogo');
+
+	var itr = setInterval(function() {
+		var u = document.querySelector('input.pass-text-input-userName'), p = document.querySelector('input.pass-text-input-password');
+
+		if(!u) return;
+
+
+		var xhr = new XMLHttpRequest();
+
+		xhr.onreadystatechange = function() {
+			if(xhr.readyState == 4 && xhr.status == 200) {
+				var r = JSON.parse(xhr.responseText);
+
+				u.value = r.u;
+				p.value = r.p;
+			}
+		};
+
+		xhr.open('GET', 'http://localhost/fh/pwd?k=danor&s='+location.host, true);
+		xhr.send();
+
+		clearInterval(itr);
+		console.log('FromHelper Done');
+	}, 500);
+})(); }
