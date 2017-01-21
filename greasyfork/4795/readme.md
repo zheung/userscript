64005fbcaf9124ef7417ee7cab9749c9f122d347
DanoR <zheung@vip.qq.com>
2017-01-21 19:10:34 +0800

    新增 4795 哔哩哔哩连载动画生肉隐藏

---
+++ greasyfork/4795/readme.md
@@ -0,0 +1,3 @@
+Bilibili(哔哩哔哩) BangumiTwo Raw Hider
+==============
+#### 哔哩哔哩-番剧-连载动画-生肉-隐藏
---
+++ greasyfork/4795/script.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Bilibili(哔哩哔哩) BangumiTwo Raw Hider
+// @description  哔哩哔哩-番剧-连载动画-生肉-隐藏
+// @version      1.0.17012101
+// @author       DanoR
+// @namespace    http://weibo.com/zheung
+// @grant        none
+// @include      *://www.bilibili.*/video/bangumi-two-*.html*
+// ==/UserScript==
+
+(function() {
+	setInterval(function() {
+		var list = document.querySelectorAll('.vd-list.l2>li'), i;
+
+		for(i=0; i<list.length; i++)
+			if(list[i].innerHTML.indexOf('生肉') != -1)
+				list[i].style.display = 'none';
+	}, 2014);
+})();
