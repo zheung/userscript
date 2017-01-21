b6b81f02d7a440e5f63bcefddf672920bb97e9c8
DanoR <zheung@vip.qq.com>
2017-01-21 19:37:01 +0800

    新增 6360 腾讯微博自动跳转大图

---
+++ greasyfork/6360/script.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Tencent-Weibo(腾讯微博) Thumbnail-InNewWindow Auto Larger
+// @description  腾讯微博-缩略图-在新窗口打开时-自动跳转大图
+// @version      1.0.17012101
+// @author       DanoR
+// @namespace    https://danor.top/
+// @grant        none
+// @include      *://*.qpic.cn/*
+// ==/UserScript==
+
+var regex = /\d*$/, num = location.href.match(/\d*$/);
+
+if (num && ~~num[0] < 2000) location.href = location.href.replace(regex, '2000');
