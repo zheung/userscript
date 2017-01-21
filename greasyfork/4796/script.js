aa5007cfe266e55e458c184491080bb62069d9e6
DanoR <zheung@vip.qq.com>
2017-01-21 15:25:58 +0800

    更新 readme; 更新 4796

--- greasyfork/4796/script.js
+++ greasyfork/4796/script.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         Weibo.com(新浪微博) Thumbnail-InNewWindow Auto Larger
 // @description  新浪微博-缩略图-在新窗口打开时-自动跳转大图
-// @version      0.4.1411130505
+// @version      1.0.17012101
 // @author       DanoR
-// @namespace    http://weibo.com/zheung
+// @namespace    https://danor.top/
 // @grant        none
-// @include      *.sinaimg.cn/*
+// @include      http?://*.sinaimg.cn/*
+// @include      /^https?://.*?\.sinaimg\.cn/.*$/
 // ==/UserScript==
 
-var regex = /square|thumbnail|bmiddle/;
-
-if (regex.test(location.href))
-	location.href = location.href.replace(regex, 'large');
+if (!/sinaimg\.cn\/large\//.test(location.href))
+	location.href = location.href.replace(/sinaimg\.cn\/.*?\//, 'sinaimg.cn/large/');
--- readme.md
+++ readme.md
@@ -2,13 +2,10 @@ DanoR-User-Scripts
 ====
 
 一些自己写的浏览器使用的用户脚本.
-用于保存以及同步到[我的GreasyFork](https://greasyfork.org/zh-CN/users/4525).
+用于保存以及同步到[我的GreasyForkhttps://greasyfork.org/zh-CN/users/4525](https://greasyfork.org/zh-CN/users/4525).
 
 Script List
 ====
-[4794]Bangumi.tv(Bangumi 番组计划) HomePage Anime Chinese-Name Shower
-Bangumi.tv(Bangumi 番组计划)-首页-动画-中文名称-显示-不智能
-
 [4796]Weibo.com(新浪微博) Thumbnail-InNewWindow Auto Larger
 新浪微博-缩略图-在新窗口打开时-自动跳转大图
 
