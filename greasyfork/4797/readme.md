c8bd0454a1b28b4c8286fee354f6bcd0089388a6
DanoR <zheung@vip.qq.com>
2017-01-21 18:23:24 +0800

    更新 4797

--- greasyfork/4797/readme.md
+++ greasyfork/4797/readme.md
@@ -2,5 +2,11 @@ Forum(Discuz! X) Base-Profile One-Clicked Filler
 ==============
 #### Discuz! X-论坛-基本资料-一键填写
 
-- 如图
+- 用于自动填写Discuz类论坛的基本资料页
+- 请自行修改在文件开头的基本资料
+- 更新脚本版本前请先备份你的基本资料
+
+- 进入"个人资料"页面后，请先按"基本资料"。
+
+- 示例图
 ![](https://raw.githubusercontent.com/zheung/userscript/master/greasyfork/4797/preview01.gif)
--- greasyfork/4797/script.js
+++ greasyfork/4797/script.js
@@ -1,50 +1,86 @@
 // ==UserScript==
 // @name         Forum(Discuz! X) Base-Profile One-Clicked Filler
 // @description  Discuz! X-论坛-基本资料-一键填写
-// @version      0.7.1411130633
+// @version      1.0.17012101
 // @author       DanoR
 // @namespace    http://weibo.com/zheung
 // @grant        none
-// @include      *op=base*
+// @include      /op=base/
 // ==/UserScript==
 
-document.querySelector('button#profilesubmitbtn.pn.pnc').parentNode.innerHTML += '<button id="fill" type="button" class="pnc pn"><strong>填写</strong></button>';
-document.querySelector('button#fill.pn.pnc').onclick = fill;
+(function() {
+	var info = {
+		birthdate: {
+			data: [1993, 8, 10],
+			privacy: 3
+		},
+		realname: {
+			data: '',
+			privacy: 3
+		},
+		gender: {
+			data: 1,
+			privacy: 3
+		},
+		bloodtype: {
+			data: 'O',
+			privacy: 3
+		},
+		lookingfor: {
+			data: '婚姻',
+			privacy: 3
+		},
+		affective: {
+			data: '单身',
+			privacy: 3
+		},
+		birthplace: {
+			data: ['广东省', '广州市'],
+			privacy: 3
+		},
+		resideplace: {
+			data: ['广东省', '广州市'],
+			privacy: 3
+		}
+	};
 
-function fill() {
-	var i;
-	var privacys = document.querySelectorAll('td.p>select');
-	for(i=0; i<privacys.length; i++) privacys[i].value = 3;
+	document.querySelector('button#profilesubmitbtn.pn.pnc').parentNode.innerHTML += '<button id="fill" type="button" class="pnc pn"><strong>填写</strong></button>';
+	document.querySelector('button#fill.pn.pnc').onclick = fill;
 
-	document.querySelector('input#realname.px').value = '';
-	document.querySelector('input#affectivestatus.px').value = '单身';
-	document.querySelector('input#lookingfor.px').value = '寂寞';
+	function fill() {
+		var i;
+		var privacys = document.querySelectorAll('td.p>select');
+		for(i=0; i<privacys.length; i++) privacys[i].value = 3;
 
-	document.querySelector('select#gender.ps').value = '1';
-	document.querySelector('select#bloodtype.ps').value = 'O';
+		document.querySelector('input#realname.px').value = info.realname.data;
+		document.querySelector('input#affectivestatus.px').value = info.affective.data;
+		document.querySelector('input#lookingfor.px').value = info.lookingfor.data;
 
-	var year = document.querySelector('select#birthyear.ps');
-	year.value = '1993'; year.onchange();
-	var month = document.querySelector('select#birthmonth.ps');
-	month.value = '8'; month.onchange();
-	document.querySelector('select#birthday.ps').value = '10';
+		document.querySelector('select#gender.ps').value = info.gender.data;
+		document.querySelector('select#bloodtype.ps').value = info.bloodtype.data;
 
-	var provinceBirth = document.querySelector('select#birthprovince.ps');
-	provinceBirth.value = '广东省';
-	provinceBirth.onchange();
-	provinceBirth.onchange();
+		var year = document.querySelector('select#birthyear.ps');
+		year.value = info.birthdate.data[0]; year.onchange();
+		var month = document.querySelector('select#birthmonth.ps');
+		month.value = info.birthdate.data[1]; month.onchange();
+		document.querySelector('select#birthday.ps').value = info.birthdate.data[2];
 
-	var provinceReside = document.querySelector('select#resideprovince.ps');
-	provinceReside.value = '广东省';
-	provinceReside.onchange();
+		var provinceBirth = document.querySelector('select#birthprovince.ps');
+		provinceBirth.value = info.birthplace.data[0];
+		provinceBirth.onchange();
 
-	var intervalID = setInterval(function()
-	{
-		var cityBirth = document.querySelector('select#birthcity.ps');
-		if(cityBirth) cityBirth.value = '广州市';
-		var cityReside = document.querySelector('select#residecity.ps');
-		if(cityReside) cityReside.value = '广州市';
+		var provinceReside = document.querySelector('select#resideprovince.ps');
+		provinceReside.value = info.resideplace.data[0];
+		provinceReside.onchange();
 
-		if(cityBirth.value == '广州市' && cityReside.value == '广州市') clearInterval(intervalID);
-	},2014);
-}
+		var intervalID = setInterval(function()
+		{
+			var cityBirth = document.querySelector('select#birthcity.ps');
+			if(cityBirth) cityBirth.value = info.birthplace.data[1];
+			var cityReside = document.querySelector('select#residecity.ps');
+			if(cityReside) cityReside.value = info.resideplace.data[1];
+
+			if(cityBirth.value == info.birthplace.data[1] && cityReside.value == info.resideplace.data[1]) clearInterval(intervalID);
+		},2014);
+	}
+})();
