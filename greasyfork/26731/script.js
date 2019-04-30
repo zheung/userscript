5356d60e7a667bbb9e3be69f634445ee9c9cb278
DanoR <zheung@vip.qq.com>
2019-05-01 02:17:53 +0800

    更新 26731;

--- greasyfork/26731/script.js
+++ greasyfork/26731/script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitter Thumbnail-InNewWindow Auto Larger
 // @description  推特-缩略图-在新窗口打开时-自动跳转大图
-// @version      1.0.1904301
+// @version      1.0.19050101
 // @author       DanoR
 // @namespace    http://weibo.com/zheung
 // @grant        none
