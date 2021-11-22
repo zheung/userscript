538ec2aaea8b7379e774ce101a7b45b0ef1c76cb
Nuogz <zheung@vip.qq.com>
2021-11-22 14:11:35 +0800

    update `Bilibili Auto Medal`

--- greasyfork/[435871]Bilibili Auto Medal/index.user.js
+++ greasyfork/[435871]Bilibili Auto Medal/index.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
-// @name         [哔哩哔哩] BiliBili Live Auto Medal
+// @name         Bilibili Live Auto Medal (哔哩哔哩)
 // @description  哔哩哔哩-直播-自动更换粉丝勋章
 // @namespace    https://danor.app/
-// @version      0.1.0-20211121
-// @author       Nuogz
+// @version      0.1.1-20211122
+// @author       Nuogz (DanoR)
 // @license      LGPL-3.0-or-later
 // @grant        none
-// @include      *live.bilibili.com/*
+// @include      /*live.bilibili.com/*/
 // ==/UserScript==
+console.log('Bilibili(哔哩哔哩) Live Auto Medal v0.1.1');
+
 
 const getMedals = async (page = 1) => {
 	const result = await (await fetch(`https://api.live.bilibili.com/xlive/app-ucenter/v1/user/GetMyMedals?page=${page}&page_size=10`, { credentials: 'include' })).json();
@@ -17,7 +19,6 @@ const getMedals = async (page = 1) => {
 		result?.data?.page_info?.total_page,
 	];
 };
-
 const getMedalsAll = async () => {
 	try {
 		const [medalsAll, total] = await getMedals();
@@ -41,26 +42,23 @@ const getMedalsAll = async () => {
 
 (async () => {
 	const idRoom = ~~location.pathname.match(/^\/(\d+)$/)?.[1];
-	if(!idRoom) { return console.error('获取[房间ID]失败'); }
+	if(!idRoom) { return; }
+
+
+	const csrf = document.cookie.split('; ').find(cookie => /^bili_jct=.*?$/.test(cookie))?.match(/^bili_jct=(.*?)$/)?.[1];
+	if(!csrf) { return console.error('未找到Cookie[bili_jct]'); }
 
 
 	const medalsAll = await getMedalsAll();
-
 
 	const idMedal = medalsAll.find(medal => medal.roomid == idRoom)?.medal_id;
 	if(!idMedal) { return console.error('未找到当前房间[粉丝勋章]'); }
 
 
-	const csrf = document.cookie.split('; ').find(cookie => /^bili_jct=.*?$/.test(cookie))?.match(/^bili_jct=(.*?)$/)?.[1];
-	if(!csrf) { return console.error('未找到[csrf]'); }
-
-
 	const resultWear = await (await fetch('https://api.live.bilibili.com/xlive/web-room/v1/fansMedal/wear', {
 		method: 'post',
 		credentials: 'include',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
-		},
+		headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;' },
 		body: Object.entries({ csrf_token: csrf, csrf, medal_id: idMedal }).map(([k, v]) => `${k}=${v}`).join('&')
 	})).json();
 
---
+++ greasyfork/[435871]Bilibili Auto Medal/readme.md
@@ -0,0 +1,14 @@
+# BiliBili Live Auto Medal (哔哩哔哩)
+进入哔哩哔哩直接间时，如果用户拥有当前直播间的勋章，会自动更换为当前用户的粉丝勋章。
+
+## 兼容性注意
+本脚本使用了`可选链操作符`语法，在较旧的浏览器下很可能会产生兼容性问题。
+- Chrome: `80`
+- Edge: `80`
+- Firefox: `74`
+- Safari: `13.1`
+
+更多浏览器兼容性信息，请查询[MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining#browser_compatibility)或其他相关网站  
+（国内浏览器请自行测试）
+
+（演示图片为GIF，如果不动请点击打开）
