666ddf0a52b8c47af633ef70269b5604af706033
DanoR <zheung@vip.qq.com>
2023-08-17 14:00:40 +0800

    renew `twitter-image-new-tab-redirect-original`

--- src/twitter-image-new-tab-redirect-original/twitter-image-new-tab-redirect-original.user.js
+++ src/twitter-image-new-tab-redirect-original/twitter-image-new-tab-redirect-original.user.js
@@ -1,23 +1,33 @@
 // ==UserScript==
-// @name         Twitter Thumbnail-InNewWindow Auto Larger
-// @description  推特-缩略图-在新窗口打开时-自动跳转大图
-// @version      2.1.19052702
-// @author       DanoR
-// @namespace    http://weibo.com/zheung
-// @grant        none
-// @include      *://pbs.twimg.com/media/*
+// @name        twitter-image-new-tab-redirect-original
+// @description 2023.08.17.13
+// @namespace   https://danor.app/
+// @version     2.2.0
+// @author      DanoR
+// @grant       none
+// @run-at      document-start
+// @match       *://pbs.twimg.com/media/*
 // ==/UserScript==
 
-if(/name=/.test(location.href)) {
-	if(!/name=orig/.test(location.href)) {
-		location.href = location.href.replace(/name=\w+/, 'name=orig');
+
+
+if(/name=/.test(location.href) && !/name=orig/.test(location.href)) {
+	let href = location.href.replace(/name=\w+/, 'name=orig');
+
+	if(/format=webp/.test(location.href)) {
+		for(const format of ['webp', 'png', 'jpg', 'bmp', 'gif']) {
+			const {status} = await fetch(
+				href.replace(/format=\w+/, `format=${format}`),
+				{ method: 'HEAD' },
+			);
+
+			if(status == 200) {
+				href = href.replace(/format=\w+/, `format=${format}`);
+
+				break;
+			}
+		}
 	}
-}
-else if(!/:orig$/.test(location.href)) {
-	if(/:\w+$/.test(location.href)) {
-		location.href = location.href.replace(/:\w+$/, ':orig');
-	}
-	else {
-		location.href += ':orig';
-	}
+
+	location.href = href;
 }
